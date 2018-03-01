09fa6e69f0d132f88138268dca783774fea581df
joao.domingues <jmmd@hotmail.com>
2018-03-01 22:08:50 +0000

    Upload solution

---
+++ MvcMovies/MvcMovies/App_Start/RouteConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovies
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                 name: "Hello",
+                 url: "{controller}/{action}/{name}/{id}"
+             );
+        }
+    }
+}
